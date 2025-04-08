466446301df603dbc5e0e5b21bb16cc05cffa181
Michael Maggiore <michaelpmaggiore@gmail.com>
2025-04-08 01:09:19 -0600

    Added astro field + win text + black hole

---
+++ Assets/UnityTechnologies/RollABall/Scripts/ShowTextOnEnter3D.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class ShowTextOnEnter3D : MonoBehaviour
+{
+    public GameObject textObject; // Drag your UI Text object here
+
+    private void Start()
+    {
+        if (textObject != null)
+            textObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            textObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            textObject.SetActive(false);
+        }
+    }
+}
